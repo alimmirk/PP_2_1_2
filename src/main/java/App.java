199a0b4e857f4a4b8f604cd6284105e0b7c9bf27
Alim <abbeyrus@student.21-school.ru>
2025-03-16 12:20:18 +0500

    v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        System.out.println(cat2.getMessage());
+        System.out.println(bean == bean2);
+        System.out.println(cat == cat2);
     }
 }
